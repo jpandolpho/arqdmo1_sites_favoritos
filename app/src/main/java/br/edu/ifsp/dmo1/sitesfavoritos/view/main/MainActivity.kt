0eb4b1538fe620ee28e92e6246385445ff8d22d6
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-12-21 11:02:48 -0300

    Refatorando projeto para modelo MVVM.

--- app/src/main/java/br/edu/ifsp/dmo1/sitesfavoritos/view/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/sitesfavoritos/view/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package br.edu.ifsp.dmo1.sitesfavoritos
+package br.edu.ifsp.dmo1.sitesfavoritos.view.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo1.sitesfavoritos.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
