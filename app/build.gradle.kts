d61bee62e4f66cfc2e70b9760f7ecfbcf962b5cb
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-12-21 15:00:50 -0300

    Atualizando sdk a pedido do AndroidStudio.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "br.edu.ifsp.dmo1.sitesfavoritos"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.edu.ifsp.dmo1.sitesfavoritos"
