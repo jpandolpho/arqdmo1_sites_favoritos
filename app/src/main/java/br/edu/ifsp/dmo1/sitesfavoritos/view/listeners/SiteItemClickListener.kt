1e209ddd63ba98454f8947625fb4d659baae6918
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-12-21 11:53:54 -0300

    Definindo interface de Listener para clicks.

---
+++ app/src/main/java/br/edu/ifsp/dmo1/sitesfavoritos/view/listeners/SiteItemClickListener.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.dmo1.sitesfavoritos.view.listeners
+
+interface SiteItemClickListener {
+    fun clickSiteItem(position:Int)
+    fun clickHearSiteItem(position:Int)
+    fun clickTrashSiteItem(position:Int)
+}
